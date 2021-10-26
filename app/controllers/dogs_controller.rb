87b12b4a62b86d41bbcdc954716a5b67969e92a0
Mehmet <mehmetk6158@gmail.com>
2021-10-25 21:50:04 -0400

    dogs create with new user by using json token

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,18 +1,19 @@
 class DogsController < ApplicationController
-  
+
   def create
 
     dog = Dog.new(
       name: params[:name],
       age: params[:age],
-      breed: params[:breed]
+      breed: params[:breed],
+      user_id: current_user.id
     )
     dog.save
 
     if current_user
       render json:dog
     else
-      render json: {message: "Sorry!You must log in first!"}
+      render json: {message: "Sorry! You must be logged in first!"}
     end
     
   end
